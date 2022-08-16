b0fd17711bad944c49240c618d3aa0bffca958f9
唐橙 <tangcheng5648713@163.com>
2022-08-16 09:24:31 +0800

    修改为API接口

--- aspnetapp/Program.cs
+++ aspnetapp/Program.cs
@@ -1,9 +1,21 @@
 using aspnetapp;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllers();
 builder.Services.AddDbContext<CounterContext>();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("V1", new OpenApiInfo
+    {
+        Title = "YYZX.API API",
+        Version = "V1",
+    });
+    // 为 Swagger JSON and UI设置xml文档注释路径
+    //c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{typeof(Startup).Assembly.GetName().Name}.xml"), true);
+});
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
@@ -11,6 +23,16 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
+app.UseSwagger();
+
+// Swagger UI
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/V1/swagger.json", "DataInterface API V1");
+    options.RoutePrefix = string.Empty;
+});
+
+
 
 app.UseStaticFiles();
 
@@ -18,6 +40,5 @@ app.UseRouting();
 
 app.UseAuthorization();
 app.MapControllers();
-app.MapRazorPages();
 
 app.Run();
